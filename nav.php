a9914648a7f4a33a82eda6314bd7b580c470eda9
Scott Schmidt <scott.schmidt1989@yahoo.com>
2020-05-11 13:25:42 -0700

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-inverse white">
+<div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <img src="images/dtec-logo.png" class="left-float d" width="100" height="50" alt="">
+      <a class="navbar-brand" href="#"></a>
+    </div>
+     <div class="center">
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="https://dtec-signup.000webhostapp.com/">Your Account</a></li>
+        <!--<li class="sm-hidden"><a  class="sell_btn green green-text z-index sm-hidden" id="sell_btn" data-toggle="modal" data-target="#sell_form"> <span class="fa fa-exchange sell_btn hideClass2" aria-hidden="true"></span> Sell Item </a></li> -->
+        <li><a href="https://datatables-ecommerence.000webhostapp.com/help.php">Help</a></li>
+             <li><a href="https://information-contact.000webhostapp.com/">Contact</a></li>
+        
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://dtec-signup.000webhostapp.com/"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="https://dtec-signup.000webhostapp.com/"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div></div>
+</nav>
